8ed6e070998adddd60606527e0d348512266476f
smartshk <smartshk98@gmail.com>
2020-10-05 18:02:58 +0900

    first week

---
+++ 1주차/script.js
@@ -0,0 +1,42 @@
+var list = document.getElementById('list');
+var button = document.getElementById('button');
+var input = document.getElementById('input');
+
+var count = 1;
+
+button.addEventListener('click', clickButton);
+
+function clickButton() {
+    
+    var li = document.createElement('li');
+  
+    li.setAttribute("id", "li"+count);
+    li.setAttribute("class", "list-group-item");
+
+    li.innerHTML = "<p>"+input.value+"</p>";
+    li.innerHTML += "<button class='btn-sm btn-secondary' type='button' onclick='remove("+count+")'>삭제</button>";
+    li.innerHTML += "<button class='btn-sm btn-secondary' type='button' onclick='update("+count+")'>수정</button>";
+
+    list.appendChild(li);
+
+    input.value = "";
+
+    count = count+1;
+}
+
+function update(count) {
+    var li = document.getElementById('li'+count);
+
+    console.log(li.firstChild.innerHTML);
+
+    input.value = li.firstChild.innerHTML;
+
+    button.innerText = "수정";
+    
+    remove(count);
+}
+
+function remove(count) {
+    var li = document.getElementById('li'+count);
+    list.removeChild(li);
+}
