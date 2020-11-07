beb1eb71017dbee6ac3d2e463bac662b07774296
Seohyun Kim <47231931+smartshk@users.noreply.github.com>
2020-11-07 23:50:39 +0900

    use antd package

--- 3주차/login-frontend/src/components/common/Button.js
+++ 3주차/login-frontend/src/components/common/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
+import Button from 'antd';
 import palette from '../../lib/styles/palette';
 
 const buttonStyle = css`
