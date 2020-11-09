e5b356fbecdb7623ae3d8e7122f31c84f33eff0a
Seohyun Kim <47231931+smartshk@users.noreply.github.com>
2020-11-09 17:41:15 +0900

    edit loginpage & registerpage

--- 4주차/practice-app/src/pages/LoginPage.js
+++ 4주차/practice-app/src/pages/LoginPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import AuthTemplate from '../components/auth/AuthTemplate';
-import LoginForm from '../containers/auth/LoginForm';
+import AuthForm from '../components/auth/AuthForm';
 
 const LoginPage = () => {
   return (
     <AuthTemplate>
-      {/* 이 부분에 로그인 폼이 들어가도록 수정*/}
-      Login Form
+      <AuthForm type="login" />
     </AuthTemplate>
   );
 };
--- 4주차/practice-app/src/pages/RegisterPage.js
+++ 4주차/practice-app/src/pages/RegisterPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import AuthTemplate from '../components/auth/AuthTemplate';
-import RegisterForm from '../containers/auth/RegisterForm';
+import AuthForm from '../components/auth/AuthForm';
 
 const RegisterPage = () => {
   return (
      <AuthTemplate>
        {/* 이 부분에 회원가입 폼이 들어가도록 수정*/}
-       Register form
+       <AuthForm type="register"/>
     </AuthTemplate>
   );
 };
